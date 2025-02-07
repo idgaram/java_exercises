d0860cca8f02eed444db3e56963205b5d47264e4
IDgaram <lorys.lellouche@gmail.com>
2025-02-07 10:58:07 -0500

    adding enhanced loop at the end

--- liste.java
+++ liste.java
@@ -13,8 +13,7 @@ class Arranger{
                 //     break;
                 //     case 2:
                 //     listeMix[i][j]= liste3[j];
-                //     break;
-                    
+                //     break;          
                 // }
                 if(i==0){
                     listeMix[i][j]= liste1[j];
@@ -38,32 +37,22 @@ public class liste {
         int liste1[] = {1,2,3};
         int liste2[] = {4,5,6};
         int liste3[] = {7,8,9};
-        // int listeMix[][] = new int[3][3];
-        // int random = (int) (Math.random() * 100);
-
 
         Arranger arranger = new Arranger();
         int listeMix[][] = arranger.inputer(liste1, liste2, liste3);
 
-
-        // for (int i = 0; i < 3; i++) {
-        //     for (int j = 0; j < 3; j++) {
-        //         listeMix[i][j] = (int) (Math.random()*100 );
-        //     }
-
-        // }
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(listeMix[i][j] + " ");
             }
             System.out.println();
-        }
+        }    
 
-        // for(int i = 0;i<liste2.length;i++){
-        // for(int n = 0;n<liste1.length;n++){
-            // System.out.println(liste1[n]);
-        // }
-        // System.out.println(liste2[i]);
-    
+        for (int n[]:listeMix){
+            for(int m:n){
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
     }
 }
