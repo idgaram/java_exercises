c9634ea717192179181b37e00c7f983fec92b39a
IDgaram <lorys.lellouche@gmail.com>
2025-02-11 11:23:58 -0500

    exemple of getter and setter

---
+++ getterSetter.java
@@ -0,0 +1,18 @@
+class Human {
+    int age;
+    String name;
+
+    public void setAge(int inputAge){
+        age = inputAge;
+    }
+
+    public int getAge(){
+        return age;
+    }
+}
+
+public class getterSetter {
+    public static void main(String[] args) {
+        
+    }
+}
