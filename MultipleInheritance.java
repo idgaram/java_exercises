eff4428eb138f56fcf46d3dc96aeb3c2358368b0
IDgaram <lorys.lellouche@gmail.com>
2025-02-18 11:06:10 -0500

    exploring ambiguity in inheritance

---
+++ MultipleInheritance.java
@@ -0,0 +1,33 @@
+class A{
+    public void x(){
+        System.out.println("in x()");
+    }
+}
+
+class B extends A{
+    public void y(){
+        System.out.println("in y()");
+    }
+}
+
+class C extends A{
+    public void z(){
+        System.out.println("in z()");
+    }
+}
+
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        
+        C c = new C();
+        B b = new B();
+
+        c.x();
+        c.z();
+        
+
+        b.x();
+        b.y();
+        
+    }
+}
