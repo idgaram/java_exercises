d505b03d47431b218af19e3500cdba2dba866bc4
IDgaram <lorys.lellouche@gmail.com>
2025-03-04 17:50:22 -0500

    adding source actions to get best practice on toString and equals

--- demo.java
+++ demo.java
@@ -20,9 +20,42 @@ class Laptop{
         return model + " " + price ;
     }
 
-    public boolean equals(Laptop that){
-return this.model.equals(that.model) && this.price == that.price;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + price;
+        result = prime * result + ((model == null) ? 0 : model.hashCode());
+        return result;
     }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Laptop other = (Laptop) obj;
+        if (price != other.price)
+            return false;
+        if (model == null) {
+            if (other.model != null)
+                return false;
+        } else if (!model.equals(other.model))
+            return false;
+        return true;
+    }
+
+//     public boolean equals(Laptop that){
+// return this.model.equals(that.model) && this.price == that.price;
+//     }
+
+
+
 }
 public class demo {
     public static void main(String[] args) {
