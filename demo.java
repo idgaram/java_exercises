1d06a6358780cdffe6f7c4a20e867cfca162af14
IDgaram <lorys.lellouche@gmail.com>
2025-03-04 17:10:12 -0500

    example of class final that stops inheritance

--- demo.java
+++ demo.java
@@ -1,8 +1,23 @@
+final class Calc{
+    public void show(){
+        System.out.println("in calc show");
+    }
+    public void add(int a, int b){
+        System.out.println(a + b);
+    }
+}
+
+class AdvancedCalc extends Calc{
+
+}
+
 public class demo {
     public static void main(String[] args) {
-        final int num = 8;
-        num = 9;
-
+        // final int num = 8;
+        // num = 9;
+        Calc obj = new Calc();
+        obj.show();
+        obj.add(5, 5);
 
        }
 }
