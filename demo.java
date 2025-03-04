fa9c7a2e6da37b93bc6d47e8492d9c0bd3c8f213
IDgaram <lorys.lellouche@gmail.com>
2025-03-04 17:07:05 -0500

    example of final variable which is a constant and cannot be reassigned

--- demo.java
+++ demo.java
@@ -1,41 +1,8 @@
-class A {
-    public void show(){
-        System.out.println("in a show");
-    }
-}
-
-class B extends A{
-    public void show(){
-        System.out.println("in b show");
-    }
-}
-
-class C extends A{
-    public void show(){
-        System.out.println("in c show");
-    }
-}
-
-class D{
-
-}
-
-
 public class demo {
     public static void main(String[] args) {
-        A a = new B();
-        
-        a.show();
+        final int num = 8;
+        num = 9;
 
-        a = new A();
-
-        a.show();
-
-        a = new C();
-
-        a.show();
-
-        // a = new D(); not possible because d doesn't extend A
 
        }
 }
