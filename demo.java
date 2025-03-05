670f1fd1010da004322c46edb7f095e65d95a286
IDgaram <lorys.lellouche@gmail.com>
2025-03-05 10:28:42 -0500

    example of upcasting a variable that is from class B but of type A

--- demo.java
+++ demo.java
@@ -1,77 +1,26 @@
-import java.lang.reflect.Field;
+class A{
+public void show1(){
+    System.out.println("in A show");
+}
+}
 
-class Laptop{
-    int price;
-    String model;
-
-    public void displayAllFields(){
-        Field[] fields = this.getClass().getDeclaredFields();
-        for (Field field : fields){
-            try {
-                System.out.println(field.getName() + " : " + field.get(this));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-    public String toString(){
-        return model + " " + price ;
-    }
-
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + price;
-        result = prime * result + ((model == null) ? 0 : model.hashCode());
-        return result;
-    }
-
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Laptop other = (Laptop) obj;
-        if (price != other.price)
-            return false;
-        if (model == null) {
-            if (other.model != null)
-                return false;
-        } else if (!model.equals(other.model))
-            return false;
-        return true;
-    }
-
-//     public boolean equals(Laptop that){
-// return this.model.equals(that.model) && this.price == that.price;
-//     }
-
-
-
+class B extends A{
+public void show2(){
+    System.out.println("in B show");
+}
 }
 public class demo {
     public static void main(String[] args) {
-        Laptop obj1 = new Laptop();
-        obj1.model = "lenovo yoga";
-        obj1.price = 1000;
+    //    double d = 4.5;
+    //    int i = (int) d;
+    //    System.out.println(i);
+       
+        A obj1 = new B();
+        obj1.show1();
+        
+        B obj2 =  new B();
+        obj2.show1();
+        obj2.show2();
 
-        Laptop obj2 = new Laptop();
-        obj2.model = "lenovo yoga";
-        obj2.price = 1000;
-
-        boolean result = obj1.equals(obj2);
-
-        System.out.println(result);
-   
-
-        // obj.displayAllFields();
        }
 }
