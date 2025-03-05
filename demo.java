a4ac4038b94edc6ded311549de41b26d6d9cede9
IDgaram <lorys.lellouche@gmail.com>
2025-03-05 10:54:49 -0500

    using wrapper classes method and converting string to int

--- demo.java
+++ demo.java
@@ -1,30 +1,16 @@
-class A{
-public void show1(){
-    System.out.println("in A show");
-}
-}
-
-class B extends A{
-public void show2(){
-    System.out.println("in B show");
-}
-}
 public class demo {
     public static void main(String[] args) {
-    //    double d = 4.5;
-    //    int i = (int) d;
-    //    System.out.println(i);
-       
-        A obj1 = new B();
-        obj1.show1();
 
-        B obj2 = (B) obj1;
-        obj2.show1();
-        obj2.show2();
+        int num = 7;
+        Integer num1 = num; //autoboxing
 
-        // B obj2 =  new B();
-        // obj2.show1();
-        // obj2.show2();
+        int num2 = num1.intValue(); //auto unboxing
+
+        System.out.println(num1);
+
+        String str = "12";
+        int numStr = Integer.parseInt(str);
+        System.out.println(numStr*2);
 
        }
 }
